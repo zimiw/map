43e0d3004bf4a756223522c9626bbb5be74610a5
jimmy <zimi_w@163.com>
2016-04-28 14:11:04 +0800

    compress html

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 //导入工具包 require('node_modules里对应模块')
 var gulp = require('gulp'), //本地安装gulp所用到的地方
-    uglify= require('gulp-uglify');
-    concat = require('gulp-concat');
+    uglify= require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    cssver = require('gulp-make-css-url-version'),
+    cssmin = require('gulp-minify-css'),
+    htmlmin = require('gulp-htmlmin'),
+    imagemin = require('gulp-imagemin');
  
 //定义一个testLess任务（自定义任务名称）
 /*gulp.task('testLess', function () {
@@ -18,9 +22,47 @@ gulp.task('jsmin', function () {
         .pipe(uglify({
             mangle: true,//类型：Boolean 默认：true 是否修改变量名
             compress: true,//类型：Boolean 默认：true 是否完全压缩
-            preserveComments: 'all' //保留所有注释
-        })).pipe(concat('all.js'))
+            preserveComments: 'license' //保留所有注释
+        }))
         .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('cssMIn',function(){
+	gulp.src('css/*.css')
+		.pipe(cssver()) //给css文件里引用文件加版本号（文件MD5）
+        .pipe(cssmin({
+            advanced: false,//类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+            compatibility: 'ie7',//保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
+            keepBreaks: true//类型：Boolean 默认：false [是否保留换行]
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('Imagemin', function () {
+    gulp.src('images/*.{png,jpg,gif,ico}')
+        .pipe(imagemin({
+            optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+            progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+            interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+            multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+        }))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('Htmlmin',function(){
+	var options = {
+        removeComments: true,//清除HTML注释
+        collapseWhitespace: true,//压缩HTML
+        collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+        minifyJS: true,//压缩页面JS
+        minifyCSS: true//压缩页面CSS
+    };
+    gulp.src('index.htm')
+        .pipe(htmlmin(options))
+        .pipe(gulp.dest('dist'));
 });
  
 /*gulp.task('jsConcat', function () {
@@ -29,4 +71,5 @@ gulp.task('jsmin', function () {
         .pipe(gulp.dest('dist/js'));
 });*/
 
-gulp.task('default',['jsmin']); //定义默认任务
+
+gulp.task('default',['jsmin','cssMIn','Imagemin','Htmlmin']); //定义默认任务
